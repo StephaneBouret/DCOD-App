c334a3150acd0744ef1b5bb070544595c613a610
Stéphane BOURET <sbouret@orange.fr>
2021-12-03 10:11:54 +0100

    Modif OrderSuccessController

--- src/Controller/OrderSuccessController.php
+++ src/Controller/OrderSuccessController.php
@@ -54,11 +54,14 @@ class OrderSuccessController extends AbstractController
             // Envoyer un email à notre client pour lui confirmer sa commande
             $assets = $request->getSchemeAndHttpHost();
             $url = $this->generateUrl('register');
+            $urlBilling = $this->generateUrl('account_order');
             $fullUrl = $assets.$url;
+            $fullUrlBilling = $assets.$urlBilling;
             $mail = new Mail();
             $content = "<h3>Nous avons reçu votre paiement.<br/>Merci !</h3><br/>Bonjour " . $order->getUser()->getFirstname() . "<br/>Merci pour votre commande !<br><br/>Toute l'équipe de Dis, comment on dit ? vous souhaite la bienvenue ! Prêt à commencer ? Il vous suffit de cliquer sur le lien ci-dessous.<br><br/>";
             $content .= "Merci de bien vouloir cliquer sur le lien suivant pour <a href='" . $fullUrl . "'>valider votre compte</a>.<br/>";
-            $content .= "N'oubliez pas d'utiliser la même adresse email pour vous enregistrer !";
+            $content .= "N'oubliez pas d'utiliser <strong>la même adresse email</strong> pour vous enregistrer !<br/>";
+            $content .= "Pour voir votre paiement, imprimer votre reçu, il vout suffit de visiter votre <a href='" . $fullUrlBilling . "'>espace de facturation</a>.";
             $mail->send($order->getUser()->getEmail(), $order->getUser()->getFirstname(), 'Votre commande de Dis, comment on dit ? est bien validée.', $content);
         }
 
